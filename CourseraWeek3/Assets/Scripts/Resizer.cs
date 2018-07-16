d21de3939dc910c4d49d33156cf51d4f7ebfd23c
Justin <brown.justin0722@gmail.com>
2018-07-16 07:59:40 -0700

    hi

---
+++ CourseraWeek3/Assets/Scripts/Resizer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resizer : MonoBehaviour {
+
+    // Used for timer support
+    const float TotalResizeSeconds = 1;
+    float elapsedResizeSeconds = 0;
+
+    //Used for resizing control
+    public float ScaleFactorPerSecond = 2;
+    public int scaleFactorSignMultiplier = -1;
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        elapsedResizeSeconds = elapsedResizeSeconds + Time.deltaTime;
+
+        if(elapsedResizeSeconds >= TotalResizeSeconds)
+        {
+            Vector3 newSpriteScale = transform.localScale;
+            newSpriteScale.x = newSpriteScale.x + (ScaleFactorPerSecond * scaleFactorSignMultiplier);
+            newSpriteScale.y = newSpriteScale.y + (ScaleFactorPerSecond * scaleFactorSignMultiplier);
+            transform.localScale = newSpriteScale;
+
+            elapsedResizeSeconds = 0;
+        }
+        //Resize the Sprite
+
+
+
+    }
+}
