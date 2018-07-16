b29d5aa4f2cce5b25d3cc081e457af75910bef10
JustinBrown <justin@cloudheadgames.com>
2018-07-15 21:17:43 -0700

    Week4Shiznat

--- 1_1 Coursera_Course/Assets/Scripts/CharacterScript.cs
+++ 1_1 Coursera_Course/Assets/Scripts/CharacterScript.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    GameObject pf_Explosion;
 
+    Timer deathTimer;
+
+    [SerializeField]
+    float minLifespan = 1;
+    [SerializeField]
+    float maxLifespan = 2;
+
+    float TeddyBearLifespanSeconds;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Get a reference to the Timer for death timer
+        deathTimer = gameObject.AddComponent<Timer>();
+        // Set the Lifespan to be a random int
+        TeddyBearLifespanSeconds = Random.Range(minLifespan, maxLifespan);
+        //Set the Duration
+        deathTimer.Duration = TeddyBearLifespanSeconds;
+        //Start the timer
+        deathTimer.Run();
+
+        // Used to spawn the bear and send him in a random direction
         const float MinImpulseForce = 3f;
         const float MaxImpulseForce = 5f;
         float angle = Random.Range(0, 2 * Mathf.PI);
@@ -14,10 +36,36 @@ public class CharacterScript : MonoBehaviour {
         float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
         GetComponent<Rigidbody2D>().AddForce(direction * magnitude, ForceMode2D.Impulse);
 	}
+
+    //void Update()
+    //{
+    //    if (deathTimer.Finished)
+    //    {
+    //        SpawnExplosion();
+    //        Destroy(gameObject);
+    //    }
+    //}
 	
     void OnCollisionEnter2D(Collision2D coll)
     {
-        print("ouch!");
+        if(coll.gameObject.tag == "TeddyBear")
+        {
+            SpawnExplosion();
+        }else
+        {
+
+            print("ouch!");
+        }
+
+
     }
 
+    void SpawnExplosion()
+    {
+        Vector3 location = transform.localPosition;
+
+        GameObject explosion;
+        explosion = Instantiate<GameObject>(pf_Explosion, location, Quaternion.identity);
+        Destroy(gameObject);
+    }
 }
---
+++ 1_1 Coursera_Course/Assets/Scripts/TeddyBearSpawner.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeddyBearSpawner : MonoBehaviour
+{
+    // Reference to the prefabs I want to spawn
+    [SerializeField]
+    GameObject pf_YellowBear;
+    [SerializeField]
+    GameObject pf_GreenBear;
+    [SerializeField]
+    GameObject pf_PurpleBear;
+
+    // spawn control
+    const float MinSpawnDelay = 1;
+    const float MaxSpawnDelay = 2;
+    Timer spawnTimer;
+
+    //Spawn location support
+    const int SpawnBorderSize = 100;
+    int minSpawnX;
+    int maxSpawnX;
+    int minSpawnY;
+    int maxSpawnY;
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        // save spawn boundaries for efficieny
+        minSpawnX = SpawnBorderSize;
+        maxSpawnX = Screen.width - SpawnBorderSize;
+        minSpawnY = SpawnBorderSize;
+        maxSpawnY = Screen.height - SpawnBorderSize;
+
+        // create and spawn timer
+        spawnTimer = gameObject.AddComponent<Timer>();
+        spawnTimer.Duration = Random.Range(MinSpawnDelay, MaxSpawnDelay);
+        spawnTimer.Run();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (spawnTimer.Finished)
+        {
+            SpawnBear();
+
+            spawnTimer.Duration = Random.Range(MinSpawnDelay, MaxSpawnDelay);
+            spawnTimer.Run();
+        }
+	}
+
+    void SpawnBear()
+    {
+        Vector3 location = new Vector3(Random.Range(minSpawnX, maxSpawnX), Random.Range(minSpawnY, maxSpawnY), -Camera.main.transform.position.z);
+        Vector3 worldLocation = Camera.main.ScreenToWorldPoint(location);
+
+        GameObject teddyBear;
+        int prefabNumber = Random.Range(0, 3);
+        if(prefabNumber == 0)
+        {
+            teddyBear = Instantiate<GameObject>(pf_YellowBear, worldLocation, Quaternion.identity);
+        }
+        else if (prefabNumber == 1)
+        {
+            teddyBear = Instantiate<GameObject>(pf_GreenBear, worldLocation, Quaternion.identity);
+        }
+        else
+        {
+            teddyBear = Instantiate<GameObject>(pf_PurpleBear, worldLocation, Quaternion.identity);
+        }
+    }
+}
